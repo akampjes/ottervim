a48c14bc64ab686a969d5bbc456719a3e600318f
Andrew Kampjes <a.kampjes@gmail.com>
2022-08-25 09:17:29 +1200

    Buffer sort ordering by most recently used

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -86,6 +86,11 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
+    buffers = {
+      ignore_current_buffer = true,
+      sort_lastused = true,
+      sort_mru = true,
+    },
   },
   extensions = {
     -- Your extension configuration goes here:
